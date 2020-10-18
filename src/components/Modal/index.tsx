a39f2b7929e39c333b137aa01dd0dcc818dcde52
yashi8484 <kobayashi@giftee.co>
2020-10-19 01:27:46 +0900

    Add Modal component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,18 @@
+import { h, FunctionComponent, ComponentChildren } from 'preact';
+import { OverlayComponent } from '../Overlay';
+
+type ModalProps = {
+  visible: boolean;
+  children: ComponentChildren;
+};
+
+export const ModalComponent: FunctionComponent<ModalProps> = ({
+  visible,
+  children,
+}) => {
+  return (
+    <OverlayComponent visible={visible}>
+      <div className="modal-card">{children}</div>
+    </OverlayComponent>
+  );
+};
