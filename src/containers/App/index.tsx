b891a5adeeb753c063827c7fd9c7b89763b27896
yashi8484 <kobayashi@giftee.co>
2020-11-01 19:30:39 +0900

    Add currentStagePuzzleSelector

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,16 +1,16 @@
 import { h, FunctionComponent } from 'preact';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { puzzleSizeState, puzzlePanelsState } from '../../atoms';
 import { AppComponent } from '../../components/App';
-import { stagePuzzleSelector } from '../../selectors';
+import {
+  currentStagePuzzleSelector,
+  stagePuzzleSelector,
+} from '../../selectors';
 
 export const App: FunctionComponent = () => {
-  const setPuzzleSize = useSetRecoilState(puzzleSizeState);
-  const setPuzzlePanels = useSetRecoilState(puzzlePanelsState);
   const puzzle = useRecoilValue(stagePuzzleSelector);
+  const setCurrentPuzzle = useSetRecoilState(currentStagePuzzleSelector);
 
-  setPuzzleSize(3);
-  setPuzzlePanels(puzzle.panels);
+  setCurrentPuzzle(puzzle);
 
   return <AppComponent />;
 };
--- src/selectors.ts
+++ src/selectors.ts
@@ -1,10 +1,35 @@
-import { selector } from 'recoil';
-import { gameState, puzzleSizeState } from './atoms';
+import { DefaultValue, selector } from 'recoil';
+import {
+  gameState,
+  puzzleAnswerPanelsState,
+  puzzlePanelsState,
+  puzzleSizeState,
+} from './atoms';
 import { getNumericPanelPuzzle } from './utils/getPuzzle';
 
 export const stagePuzzleSelector = selector({
   key: 'stagePuzzleSelector',
-  get: ({ get }) => getNumericPanelPuzzle(get(puzzleSizeState)),
+  get: () => getNumericPanelPuzzle(3),
+});
+
+export const currentStagePuzzleSelector = selector<Puzzle>({
+  key: 'currentStagePuzzleSelector',
+  get: ({ get }) => ({
+    answerPanels: get(puzzleAnswerPanelsState),
+    panels: get(puzzlePanelsState),
+    size: get(puzzleSizeState),
+  }),
+  set: ({ set, reset }, newValue) => {
+    if (newValue instanceof DefaultValue) {
+      reset(puzzleAnswerPanelsState);
+      reset(puzzlePanelsState);
+      reset(puzzleSizeState);
+    } else {
+      set(puzzleAnswerPanelsState, newValue.answerPanels);
+      set(puzzlePanelsState, newValue.panels);
+      set(puzzleSizeState, newValue.size);
+    }
+  },
 });
 
 export const isGameStateNotReadySelector = selector({
