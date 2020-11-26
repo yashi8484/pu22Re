cfc2ded8ffce23dbb11e7753efa5d50be4ba033d
yashi8484 <kobayashi@giftee.co>
2020-11-26 23:22:06 +0900

    Add tailwind settings for removing unused styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   future: {
     // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
+    purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ['./src/**/*.tsx'],
   theme: {
     extend: {
       colors: {
